8a78081861166076d5bb4a7b6a35f4ade9eaeab8
Oleksii Ovchynnikov <Oleksii.Ovchynnikov@genesys.com>
2020-02-03 18:01:00 +0200

    make it better

---
+++ prodapp.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+const Cryptr = require('cryptr');
+cryptr = new Cryptr('oleksii');
+const express = require("express");
+		app = express(),
+		http = require("http").Server(app).listen(80),
+		upload = require("express-fileupload");
+app.use(upload())
+app.use(express.static(__dirname + '/public'));
+console.log("Server started")
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/index2.html");
+})
+app.post("/encode",function(req,res){
+	if(req.files){
+		const file = req.files.filename,
+		filename = req.files.filename;
+		let dataFromFile = filename.data;
+	 	const mykey = cryptr.encrypt(dataFromFile);
+			fs.appendFileSync("./EncryptedFiles/"+"Encrypted-" + filename.name, mykey +'\n');
+			res.download("./EncryptedFiles/"+"Encrypted-" + filename.name);
+	}
+ })
+ 
+
+app.post("/decode",function(req,res){
+	if(req.files){
+		
+		const file = req.files.filename,
+		filename = req.files.filename;
+		console.log(filename.data);
+	 	let dataFromFile = filename.data;
+	 	const mykey = cryptr.decrypt(dataFromFile);
+		
+				  fs.appendFileSync("./DecryptedFiles/"+"Decrypted-" + filename.name, mykey +'\n');
+				  res.download("./DecryptedFiles/"+"Decrypted-" + filename.name);
+					
+	}
+ })
+
+ 
+
